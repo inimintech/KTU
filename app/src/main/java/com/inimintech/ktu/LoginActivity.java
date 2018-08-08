3a27d45052e669bb4e76e633bce2477891b9b659
inimintech <inimintech@gmail.com>
2018-08-08 18:57:28 +0530

    added phone auth activity need to test

--- app/src/main/java/com/inimintech/ktu/LoginActivity.java
+++ app/src/main/java/com/inimintech/ktu/LoginActivity.java
@@ -1,13 +1,51 @@
 package com.inimintech.ktu;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class LoginActivity extends AppCompatActivity {
+    private EditText editTextMobile;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        editTextMobile = findViewById(R.id.editTextMobile);
+        mAuth = FirebaseAuth.getInstance();
+
+        findViewById(R.id.buttonContinue).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String mobile = editTextMobile.getText().toString().trim();
+
+                if(mobile.isEmpty() || mobile.length() < 10){
+                    editTextMobile.setError("Enter a valid mobile");
+                    editTextMobile.requestFocus();
+                    return;
+                }
+
+                Intent intent = new Intent(LoginActivity.this, VerifyPhnoActivity.class);
+                intent.putExtra("mobile", mobile);
+                startActivity(intent);
+            }
+        });
+
     }
 }
