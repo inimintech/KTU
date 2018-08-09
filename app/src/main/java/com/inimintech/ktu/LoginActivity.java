efdc8ef2877c5227e4d47405db57ffa5385f3a5e
inimintech <inimintech@gmail.com>
2018-08-09 15:48:29 +0530

    auto populate mobile number

--- app/src/main/java/com/inimintech/ktu/LoginActivity.java
+++ app/src/main/java/com/inimintech/ktu/LoginActivity.java
@@ -1,9 +1,14 @@
 package com.inimintech.ktu;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.TelephonyManager;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
@@ -29,13 +34,15 @@ public class LoginActivity extends AppCompatActivity {
         editTextMobile = findViewById(R.id.editTextMobile);
         mAuth = FirebaseAuth.getInstance();
 
+        editTextMobile.setText(getMyPhoneNO());
+
         findViewById(R.id.buttonContinue).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
                 String mobile = editTextMobile.getText().toString().trim();
 
-                if(mobile.isEmpty() || mobile.length() < 10){
+                if (mobile.isEmpty() || mobile.length() < 10) {
                     editTextMobile.setError("Enter a valid mobile");
                     editTextMobile.requestFocus();
                     return;
@@ -48,4 +55,20 @@ public class LoginActivity extends AppCompatActivity {
         });
 
     }
+
+    private String getMyPhoneNO() {
+        try {
+            TelephonyManager tMgr = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_SMS) != PackageManager.PERMISSION_GRANTED
+                    && ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_NUMBERS) != PackageManager.PERMISSION_GRANTED
+                    && ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+                return "";
+            }
+            return tMgr.getLine1Number();
+        }catch (Exception e){
+            return "";
+        }
+    }
+
+
 }
