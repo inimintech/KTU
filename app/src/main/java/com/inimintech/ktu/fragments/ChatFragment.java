ab4e298b0a8bed8f3181d846995052b13be3eeef
inimintech <inimintech@gmail.com>
2018-08-29 19:24:28 +0530

    added bottom navigation activity

--- app/src/main/java/com/inimintech/ktu/fragments/ChatFragment.java
+++ app/src/main/java/com/inimintech/ktu/fragments/ChatFragment.java
@@ -1,23 +1,17 @@
 package com.inimintech.ktu.fragments;
 
-import android.content.Context;
+import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.LinearLayout;
-import android.widget.ScrollView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -25,17 +19,11 @@ import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 import com.inimintech.ktu.ChatActivity;
 import com.inimintech.ktu.R;
-import com.inimintech.ktu.VerifyPhnoActivity;
-import com.inimintech.ktu.activity.Main2Activity;
-import com.inimintech.ktu.adaptor.ChatAdapter;
-import com.inimintech.ktu.data.Chat;
 import com.inimintech.ktu.data.Discussion;
 import com.inimintech.ktu.services.FirestoreServices;
 
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 /*
@@ -43,7 +31,8 @@ import java.util.Map;
  * @Created on  11 Aug 2018
  */
 
-public class ChatFragment extends Fragment {
+public class ChatFragment extends Fragment implements SwipeRefreshLayout.OnRefreshListener {
+    SwipeRefreshLayout swipeLayout;
 
     private static final String TAG = ChatFragment.class.getName();
     private LinearLayout topicsList;
@@ -63,13 +52,37 @@ public class ChatFragment extends Fragment {
         super.onCreate(savedInstanceState);
     }
 
+    @SuppressLint("ResourceAsColor")
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View root =  inflater.inflate(R.layout.fragment_chat, container, false);
-        discussions = new HashMap<>();
         topicsList = root.findViewById(R.id.topicsList);
 
+        swipeLayout = (SwipeRefreshLayout) root.findViewById(R.id.swipe_container);
+        swipeLayout.setOnRefreshListener(this);
+        swipeLayout.setColorSchemeColors(android.R.color.holo_green_dark,
+                android.R.color.holo_red_dark,
+                android.R.color.holo_blue_dark,
+                android.R.color.holo_orange_dark);
+
+        swipeLayout.setRefreshing(true);
+
+        loadTopics();
+
+        /*topicsList.addView(createViews(topicsList));
+        topicsList.addView(createViews(topicsList));
+        topicsList.addView(createViews(topicsList));
+        topicsList.addView(createViews(topicsList));*/
+
+
+        return root;
+
+    }
+
+    private void loadTopics(){
+        topicsList.removeAllViews();
+        discussions = new HashMap<>();
         FirestoreServices.ourdb.collection("Discussions")
                 .get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
             @Override
@@ -81,21 +94,13 @@ public class ChatFragment extends Fragment {
                         topicsList.addView(getView(topicsList, d, document.getId()));
                         Log.d(TAG, document.getId() + " => " + document.getData());
                     }
+                    swipeLayout.setRefreshing(false);
                 } else {
                     Log.d(TAG, "Error getting documents: ", task.getException());
                 }
 
             }
         });
-
-        /*topicsList.addView(createViews(topicsList));
-        topicsList.addView(createViews(topicsList));
-        topicsList.addView(createViews(topicsList));
-        topicsList.addView(createViews(topicsList));*/
-
-
-        return root;
-
     }
 
     public View getView(ViewGroup parent, Discussion d, String tag){
@@ -127,4 +132,9 @@ public class ChatFragment extends Fragment {
     }
 
 
+    @Override
+    public void onRefresh() {
+
+        loadTopics();
+    }
 }
